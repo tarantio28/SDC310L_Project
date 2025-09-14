1d1fc9d6763f8d12eb48bb5733619df056d96790
Clint Hester <clihes7837@students.ecpi.edu>
2025-09-14 14:17:15 -0400

    Initial Commit

--- model/database.php
+++ model/database.php
@@ -1,30 +1,26 @@
 <?php
+namespace Model;
+
+use mysqli;
+
 class Database {
     private $host = "localhost";
     private $dbname = "store_db";
-    private $username = "root";
-    private $password = "";
+    private $username = "root";   
+    private $password = "";       
     private $conn;
 
-    public function __construct() {
-        $this->connect();
-    }
-
-    private function connect() {
-        $this->conn = new mysqli(
-            $this->host,
-            $this->username,
-            $this->password,
-            $this->dbname
-        );
-
-        if ($this->conn->connect_error) {
-            die("Database connection failed: " . $this->conn->connect_error);
-        }
-    }
-
     public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+            if ($this->conn->connect_error) {
+                die("Database connection failed: " . $this->conn->connect_error);
+            }
+        } catch (\Exception $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
         return $this->conn;
     }
 }
-?>
+
--- view/products.php
+++ view/products.php
@@ -1,32 +1,32 @@
 <?php
-require_once "../model/database.php";
+require_once __DIR__ . "/../model/database.php";
+
+use Model\Database;
 
 $db = new Database();
 $conn = $db->getConnection();
 
-$sql = "SELECT * FROM products";
-$result = $conn->query($sql);
+if (!$conn) {
+    die("Database connection failed.");
+}
+
+$result = $conn->query("SELECT * FROM products");
+
+echo "<h1>Product List</h1>";
+echo "<table border='1' cellpadding='8'>";
+echo "<tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Stock</th></tr>";
+
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $row['product_id'] . "</td>";
+    echo "<td>" . $row['product_name'] . "</td>";
+    echo "<td>" . $row['description'] . "</td>";
+    echo "<td>$" . $row['price'] . "</td>";
+    echo "<td>" . $row['stock'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+$conn->close();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product List</title>
-</head>
-<body>
-    <h1>Available Products</h1>
-    <table border="1" cellpadding="10">
-        <tr>
-            <th>Name</th><th>Description</th><th>Price</th><th>Stock</th>
-        </tr>
-        <?php while($row = $result->fetch_assoc()) { ?>
-        <tr>
-            <td><?php echo $row["product_name"]; ?></td>
-            <td><?php echo $row["description"]; ?></td>
-            <td>$<?php echo $row["price"]; ?></td>
-            <td><?php echo $row["stock"]; ?></td>
-        </tr>
-        <?php } ?>
-    </table>
-</body>
-</html>
